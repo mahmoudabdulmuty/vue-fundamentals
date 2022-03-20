dfb66b4c0e3e9209bbc624ee74488816b85616de
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-03-20 04:12:38 +0200

    figuring out what's goin on

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>Task runner</h1>
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
