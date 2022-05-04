ce61e08da0fddd7a62014da785428141a36d2940
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-05-04 09:13:50 +0200

    Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -215,6 +215,28 @@
   <h2 v-once>{{ name }}</h2>
   <button @click="name = 'Batman'">Change name</button>
   <h2 v-pre>{{ name }}</h2>
+
+  <!-- Computed Properties -->
+  <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
+  <h2>Fullname - {{ fullName }}</h2>
+  <h2>
+    Total - {{ items.reduce((total, curr) => (total = total + curr.price), 0) }}
+  </h2>
+  <h2>Computed Total - {{ total }}</h2>
+  <h2>Method Total - {{ getTotal() }}</h2>
+  <button @click="items.push({ id: 4, title: 'Keyboard', price: 50 })">
+    Add item
+  </button>
+  <div>
+    <input type="text" v-model="country" />
+  </div>
+  <template v-for="item in items" :key="item.id">
+    <h2 v-if="item.id === 1">{{ item.title }} - {{ item.price }}</h2>
+  </template>
+  <h2 v-for="item in expensiveItems" :key="item.id">
+    {{ item.title }} - {{ item.price }}
+  </h2>
+  <button @click="changeFullName">Change fullname</button>
 </template>
 
 <script>
