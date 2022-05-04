e4b73f5e2c315631aff5c797b45cbc5ca9819be3
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-05-04 08:28:12 +0200

    Form Handling (Part 2)

--- src/App.vue
+++ src/App.vue
@@ -137,6 +137,78 @@
         <option value="singapore">Singapore</option>
       </select>
     </div>
+    <div>
+      <input
+        id="remoteWork"
+        type="checkbox"
+        v-model="formValues.remoteWork"
+        true-value="yes"
+        false-value="no"
+      />
+      <label for="remoteWork">Open to remote work?</label>
+    </div>
+    <div>
+      <label>Skill set</label>
+      <input
+        type="checkbox"
+        id="html"
+        value="html"
+        v-model="formValues.skillSet"
+      />
+      <label for="html">HTML</label>
+      <input
+        type="checkbox"
+        id="css"
+        value="css"
+        v-model="formValues.skillSet"
+      />
+      <label for="css">CSS</label>
+      <input
+        type="checkbox"
+        id="javascript"
+        value="javascript"
+        v-model="formValues.skillSet"
+      />
+      <label for="javascript">JavaScript</label>
+    </div>
+    <div>
+      <label>Years of Experience</label>
+      <input
+        type="radio"
+        id="0-2"
+        value="0-2"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="0-2">0-2</label>
+      <input
+        type="radio"
+        id="3-5"
+        value="3-5"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="3-5">3-5</label>
+      <input
+        type="radio"
+        id="6-10"
+        value="6-10"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="6-10">5-10</label>
+      <input
+        type="radio"
+        id="10+"
+        value="10+"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="10+">10+</label>
+    </div>
+    <div>
+      <label for="age">Age</label>
+      <input type="number" id="age" v-model.number="formValues.age" />
+    </div>
+    <div>
+      <button>Submit</button>
+    </div>
   </form>
 </template>
 
