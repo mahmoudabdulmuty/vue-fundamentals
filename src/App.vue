0f84e48ef2d001066e1d095803bc75951b7f1125
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-05-04 08:17:47 +0200

    Form Handling (Part 1)

--- src/App.vue
+++ src/App.vue
@@ -104,6 +104,40 @@
     <button @click="increment(5)">Increment 5</button>
     <button @click="decrement(5)">Decrement 5</button>
   </div>
+
+  <!-- Form Handling -->
+  <div>
+    <pre>
+      {{ JSON.stringify(formValues, null, 2) }}
+    </pre>
+  </div>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model.trim="formValues.name" />
+    </div>
+    <div>
+      <label for="profile">Profile Summary</label>
+      <textarea id="profile" v-model="formValues.profileSummary" />
+    </div>
+    <div>
+      <label for="country">Country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+    <div>
+      <label for="job-location">Job location</label>
+      <select id="job-location" v-model="formValues.jobLocation" multiple>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+  </form>
 </template>
 
 <script>
