759315e5702f9efd4008ff97dbdc36bc70a6cb81
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-05-04 18:01:32 +0200

    components

---
+++ src/components/Greet.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>
+    Hello
+    {{ username }} and age is {{ userAge }}
+  </h2>
+</template>
+
+<script>
+export default {
+  name: "Greet",
+  props: ["username", "userAge"],
+};
+</script>
+
+<style scoped>
+</style>
